620c89b6b7391d739a375d72a9c482ee62ff6ee1
javoh1rqaxramonov <ubotirov22@.com>
2024-07-20 18:33:46 +0500

    finished

---
+++ index.js
@@ -0,0 +1,251 @@
+// 1-masala
+// let a = 12 ;
+// if (a>18) {
+//     a="Siz katta yoshdagisiz";
+// } else {
+//    a="Siz hali yoshsiz" 
+// }
+// console.log(a);
+
+// 2-masala 
+// let a=12;
+// if (a%2==0) {
+//     a ="juft";
+// } else if(a%2==1){
+//     a="toq";
+// }
+// console.log(a);
+
+// 3-masala
+// let a=2;
+// if (a>0) {
+//     a="musbat";
+// } else {
+//     a="manfiy"
+// }
+// console.log(a);
+
+// 4-masala
+// let a =13;
+// if (a>20) {
+//     a="yengil kiyining"
+// } else {
+//     a="qalin kiyining"
+// }
+// console.log(a);
+
+// 5-masala 
+// let a =32;
+// if (a>90) {
+//     a="alo";
+// } else if(a>=70 || a<=89){
+//     a= "yaxshi";
+// } else if(a>=50 || a<=69){
+//     a="qoniqarli";
+// } else if (a<50) {
+//     a="qachada yashaysan";
+// } 
+// console.log(a);
+
+// 6-masala 
+// let name = "ali";
+// if (name =name) {
+//     name = "salom ali";
+// } else {
+//     name ="salom mehmon"
+// }
+// console.log(name);
+
+// 7-masala 
+// let a =99;
+// if (a>=100) {
+//     a="Chegirma qollanildi"
+// } else {
+//     a="Chegirma mavjud emas"
+// }
+// console.log(a);
+
+// 1-masala 
+// let a = 34 ;
+// a>25 ? console.log("issiq") : console.log("sovuq");
+// console.log(a);
+
+// 2-masala 
+// let a = 49;
+// a>50 ? console.log("otdi") : console.log("yeqildi");
+// console.log(a);
+
+// 3-masala 
+// let a = 9;
+// a>0 ? console.log("musbat") : console.log("manfiy");
+// console.log(a);
+
+// 1-masala 
+// for (let i=1;i<=10;i++){
+//     console.log(i);
+// } 
+
+// 2-masala 
+// for (let i=10;i>=1;i--){
+//     console.log(i);
+// } 
+
+// 3-masala 
+// for (let i = 1; i <= 100; i++) {
+//     if (i % 2 == 0) {
+//       console.log(i);
+//     }
+//}
+
+// 4-masala 
+// for (let i = 1; i <= 100; i++) {
+//     if (i % 2 == 1) {
+//       console.log(i);
+//     }
+//   }
+
+// 5-masala 
+// let sum = 0;
+// for (let i = 1; i <= 10; i++) {
+//   sum += i;
+// }
+// console.log(sum);
+
+// 6-masala 
+// let sum = 1;
+// for (let i = 1; i <= 10; i++) {
+//   sum *= i;
+// }
+// console.log(sum);
+
+// 7-masala 
+// let sum = 1;
+// let a=4;
+// for (let i = 1; i <= a; i++) {
+//   sum *= i;
+// }
+// console.log(sum);
+
+// 8-masala 
+// for (let i = 1; i <= 50; i++) {
+//   console.log(i**2);
+// }
+
+// 9-masala 
+// for (let i = 1; i <= 20; i++) {
+//   console.log(i**3);
+// }
+
+// 10-masala 
+// let a =1000;
+// let sum =0;
+// for(let i=1;i<=1000;i++){
+//     sum+=i;
+// }
+// console.log(sum);
+
+// 11-masala 
+// let a = 5;
+// for(let i =1;i<=5;i++){
+//     console.log("salom");
+// }
+
+// 12-masala 
+// let a =100;
+// let sum =0;
+// for (let i=1;i<=100;i++){
+//     console.log(i*2);
+// }
+
+// 13-masala 
+// let a =12;
+// let res ;
+// for(let i=1; i<=12;i++){
+//     console.log(2**i);
+// }
+
+// 14-masala
+// let a =5;
+// let sum;
+// for (let i=1;i<=a;i++){
+//   sum=i*i
+// }
+// console.log(sum);
+
+// 15-masala 
+// let num  = 122;
+// let s = "a";
+// for (let i =97;i<=122;i++){
+// console.log(String.fromCharCode(i));
+
+// 1-masala 
+// let a =4;
+// let b=5;
+// if (a%2==0 && b%2==0 || a%2==1 && b%2==1) {
+//     console.log(true);
+// } else{
+//     console.log(false);
+// }
+
+// 2-masala 
+// let a =4;
+// let b=5;
+// let c =3;
+// if (a>0 && b>0 && c>0) {
+//     console.log(true);
+// } else{
+//     console.log(false);
+// }
+
+// 3-masala 
+// let a =4;
+// let b=5;
+// let c =3;
+// if (a>0) {
+//     console.log(true);
+// } else {
+//     console.log(false);
+// }
+// if (b>0) {
+//     console.log(true);
+// } else {
+//     console.log(false);
+// }
+// if (c>0) {
+//     console.log(true);
+// } else {
+//     console.log(false);
+// }
+
+// 4-masala 
+// let a =4;
+// let b=5;
+// let c =3;
+// if (a>0 && b<0 && c<0 || a<0 && b>0 && c<0 || a<0 && b<0 && c>0) {
+//     console.log(true);
+// } else{
+//     console.log(false);
+// }
+
+// 5-masala 
+// let a =4;
+// let b=5;
+// let c =3;
+// if (a>0 && b>0 && c<0 || a<0 && b>0 && c>0 || a>0 && b<0 && c>0) {
+//     console.log(true);
+// } else{
+//     console.log(false);
+// }
+
+// 6-masala 
+// let a=54;
+// let res;
+// if (a%2==0 && a>=10 && a<=99) {
+//     console.log(true);
+// } else {
+//     console.log(false);
+// }
+  
+
+
+
